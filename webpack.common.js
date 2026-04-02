1bc921461edcd4ec79d14a2fe17a772352564b10
Brian Zhang <zhang.15977@osu.edu>
2026-04-02 11:21:22 -0400

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+    entry: {
+        app: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        title: 'Production',
+        template: "./src/template.html",
+    }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(import.meta.dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        {
+            test: /\.html$/i,
+            use: ["html-loader"],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+        },
+        ],
+  },
+ };
